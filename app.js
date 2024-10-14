7d089354d26066c102a70ba2c49aecd084410b82
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-10-14 08:36:00 +0530

    episode-01

--- app.js
+++ app.js
@@ -15,21 +15,22 @@
  */
 
 const parent = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', {}, 'This is h1 tag'),
-    React.createElement('h2', {}, 'This is h2 tag')
-  ]),
-  React.createElement('div', { id: 'child2' }, [
-    React.createElement('h1', {}, 'This is h1 tag'),
-    React.createElement('h2', {}, 'This is h2 tag')
+    React.createElement('div', { id: 'child1' }, [
+      React.createElement('h1', {}, 'This is h1 tag'),
+      React.createElement('h2', {}, 'This is h2 tag')
+    ]),
+    React.createElement('div', { id: 'child2' }, [
+      React.createElement('h1', {}, 'This is h1 tag'),
+      React.createElement('h2', {}, 'This is h2 tag')
+    ])
   ])
-])
-
-const heading = React.createElement(
-  'h1',
-  { id: 'heading', xyz: 'abc' },
-  'Hello world from react!'
-)
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(parent)
+  
+  const heading = React.createElement(
+    'h1',
+    { id: 'heading', xyz: 'abc' },
+    'Hello world from react!'
+  )
+  const root = ReactDOM.createRoot(document.getElementById('root'))
+  
+  root.render(parent)
+  
